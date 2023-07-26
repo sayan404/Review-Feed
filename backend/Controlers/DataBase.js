065defc9c24a1744948a78fa19f5d9bfca627dde
sayan404 <95490367+sayan404@users.noreply.github.com>
2023-07-26 11:28:40 +0530

    upated db connection
    upated db connection

--- backend/Controlers/DataBase.js
+++ backend/Controlers/DataBase.js
@@ -1,7 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDataBase = () => {
-    mongoose.connect(`${process.env.MONGODB_URL}`).then((data) => {
+const connectDataBase = async () => {
+  await mongoose.connect(process.env.MONGODB_URL ,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((data) => {
         console.log(`Mongodb connected with server: ${data.connection.host}`);
 }).catch((err) => {
         throw err;
